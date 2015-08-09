23352ee7099a72df635dccd7796231efb5341cf0
hkawabata <meiwaku_mail_iyaaaaa@yahoo.co.jp>
2015-08-09 10:02:55 +0900

    コメントを整理

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -11,44 +11,48 @@
 ;						 )
 ;                        load-path))
 
-;;; パッケージ管理に melpa を追加
+;;;********** パッケージ管理に melpa, marmalade を追加 **********
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-
+;;;********** パッケージの自動インストール **********
 (require 'cl)
 (defvar my-packages
   '(flycheck
-	js2-mode php-mode
-	auto-complete
-	git-gutter
-	;;gnuplot-mode markdown-mode scala-mode
-	)
+    auto-complete
+    git-gutter
+    col-highlight
+    js2-mode php-mode
+    ;; gnuplot-mode markdown-mode scala-mode
+    ;; このリストにあるパッケージがインストールされる
+    )
   "A list of packages to ensure are installed at launch.")
-
 (defun my-packages-installed-p ()
   (loop for p in my-packages
-	when (not (package-installed-p p)) do (return nil)
-	finally (return t)))
-
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
 (unless (my-packages-installed-p)
   ;; check for new packages (package versions)
   (package-refresh-contents)
   ;; install the missing packages
   (dolist (p my-packages)
-	(when (not (package-installed-p p))
-	  (package-install p))))
+    (when (not (package-installed-p p))
+      (package-install p))))
 
 
-;;; スクロールの設定
+;;;********** スクロールの設定 **********
 (setq scroll-conservatively 35
       scroll-margin 3  ; スクロールするときの上下のマージン
-	  scroll-step 1)   ; 1行ずつスクロール
+      scroll-step 1)   ; 1行ずつスクロール
 
 
-;;; git で管理しているファイルについて、変更・追加・削除箇所を表示
+;;********** オートインデントでスペースを使う **********
+ (setq-default indent-tabs-mode nil)
+
+
+;;;********** git で管理しているファイルについて、変更・追加・削除箇所を表示 **********
 ;;; 【要】 git-gutter
 (global-git-gutter-mode t)
 (git-gutter:linum-setup)
@@ -59,7 +63,8 @@
 (require 'auto-complete)
 (global-auto-complete-mode t)
 
-;;;; 自動コードチェック
+
+;;;********** 自動コードチェック **********
 ;;; 【要】flycheck
 ;;;;     対応言語（確認済み）: C, C++, elisp, HTML, Perl, Python, Ruby, JS
 ;;;;     対応言語（未確認）: CSS, PHP, ShelScripts, Tex, XML
@@ -67,68 +72,57 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-;;;; メニューバーを非表示
-;;;; not express menu bar
+;;;********** メニューバーを非表示 **********
 (menu-bar-mode -1)
 
-;;;; 色などの初期設定
-;;;; Initial color
+
+;;;********** 色などの初期設定 **********
 (setq default-frame-alist
       (append (list
-		   '(foreground-color . "snow") ;; 文字色
-		   '(background-color . "black") ;; 背景色
-		   ;'(border-color     . "yellow") ;; 縁の色
-	       ;'(mouse-color      . "white")
-	       ;'(cursor-color     . "white") ; 効いてない
-	       ;'(cursor-type      . box) ; 効いてない
-	       ;'(cursor-height    . 4) ; 効いてない
+               '(foreground-color . "snow") ;; 文字色
+               '(background-color . "black") ;; 背景色
+               )
+              default-frame-alist))
 
-	       ;'(menu-bar-lines . 1)
-	       ;'(font . "my-fontset")
-	       ;'(vertical-scroll-bars . nil) ;;スクロールバーを消す
-	       ;'(width . 100) ;; ウィンドウ幅
-	       ;'(height . 40) ;; ウィンドウの高さ
-	       ;'(top . 60)   ;; 左上隅のy座標
-	       ;'(left . 140) ;; 左上隅のx座標
-	       )
-	      default-frame-alist))
 
-;;;; 背景透過度
-;;;; 数値は0(透明) - 100(非透明), (アクティブ時 非アクティブ時)
-;(when (featurep 'carbon-emacs-package)
-;  (add-to-list 'default-frame-alist '(alpha . (100 50)))
-;)
-
-;;;; 選択領域の色
-;;;; color of selected region
+;;;********** 選択領域の色 **********
 (set-face-background 'region "white")
 (set-face-foreground 'region "black")
-;;;; コメントの色
-;;;; color of comments
+
+;;;********** コメントの色 **********
 (set-face-foreground 'font-lock-comment-face "red")
 
-;; tab ではなく space を使う
-;(setq-default indent-tabs-mode nil)
-;; tab 幅を 4 に設定
+;;********** tab 幅を 4 に設定 **********
 (setq-default tab-width 4)
-;; バッファの最後の行で next-line しても新しい行を作らない
-;(setq next-line-add-newlines nil)
 ;; narrowing を禁止
 ;(put 'narrow-to-region 'disabled nil)
 
-;;; 現在の行を目立たせる
+
+;;;********** 現在の列をハイライト **********
+(require 'col-highlight)
+;; 常時ハイライト
+(column-highlight-mode 1)
+;; 1秒間何もしないとハイライト
+;; (toggle-highlight-column-when-idle 1)
+;; (col-highlight-set-interval 1)
+;; ハイライトの色を指定
+(custom-set-faces
+ '(col-highlight((t (:background "blue")))))
+
+;;;********** 現在の行をハイライト **********
 (global-hl-line-mode)
 (custom-set-faces'(hl-line ((t (:background "blue")))))
 
-;;; 列番号を表示
+
+;;;********** 列番号を表示 **********
 (column-number-mode t)
 
-;;; カーソル位置を保存
+;;;********** カーソル位置を保存 **********
 (require 'saveplace)
 (setq-default save-place t)
 
 
-;;; 左側に行番号を表示
+;;;********** 左側に行番号を表示 **********
 (require 'linum)
 (global-linum-mode t)
 ;; linum の表示フォーマット
@@ -139,15 +133,15 @@
  '(linum ((t (:inherit (shadow default) :background "Gray23")))))
 
 
-;; 括弧の対応表示
+;;********** 括弧の対応表示 **********
 (show-paren-mode t)
 (setq show-paren-style 'mixed)
-;(setq show-paren-style 'expression)     ;;常に括弧内に色を付けたいときはこっち
 (set-face-background 'show-paren-match-face "yellow")
 (set-face-foreground 'show-paren-match-face "black")
 (set-face-background 'show-paren-mismatch-face "red")
 (set-face-foreground 'show-paren-mismatch-face "white")
-
+;;常に括弧内に色を付けたいとき
+;; (setq show-paren-style 'expression)
 
 
 ;;******************************************************************
